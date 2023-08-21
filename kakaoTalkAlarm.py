b6e27f5da95495c63afa9862b1e9a581b9bd7f98
hyeonjeong ko <72601276+hyeonjeong-ko@users.noreply.github.com>
2023-08-21 15:25:32 +0900

    Update kakaoTalkAlarm.py

--- kakaoTalkAlarm.py
+++ kakaoTalkAlarm.py
@@ -11,6 +11,22 @@ redirect_uri = os.environ.get('REDIRECT_URI') #ㅇㅇ
 code_key = os.environ.get('CODE_KEY') #이건 어디에 쓰는 물건인고...?
 msg_template = os.environ.get('MSG_TEMPLATE') #ㅇㅇ
 
+# Get user information from environment variables
+user_name = os.environ.get('USER_NAME')
+commit_time = os.environ.get('COMMIT_TIME')
+commit_message = os.environ.get('COMMIT_MESSAGE')
+from_branch = os.environ.get('FROM_BRANCH')
+to_branch = os.environ.get('TO_BRANCH')
+
+
+# Print the user information
+print("User Name:", user_name)
+print("Commit Time:", commit_time)
+#print("New Commit Time:", new_commit_time)
+print("Commit Message:", commit_message)
+print("From Branch:", from_branch)
+print("To Branch:", to_branch)
+
 if event_name == 'Pull Request':
     if merged_status == 'true':
         print("This is a merged Pull Request.")
@@ -52,52 +68,20 @@ if send_to_function == 'send_to_me':
         "Authorization": "Bearer " + access_token # {access token}
     }
     
-    # Get user information from environment variables
-    user_name = os.environ.get('USER_NAME')
-    commit_time = os.environ.get('COMMIT_TIME')
-    commit_message = os.environ.get('COMMIT_MESSAGE')
-    from_branch = os.environ.get('FROM_BRANCH')
-    to_branch = os.environ.get('TO_BRANCH')
-    
-    # pull request과정에서의 시간 변수는 시차 존재
-    # Commit time을 문자열에서 datetime 객체로 변환
-    #commit_time_datetime = datetime.strptime(commit_time, '%Y-%m-%d %H:%M:%S %z')
-    
-    # 9시간을 추가하여 새로운 datetime 객체 생성
-    #new_commit_time_datetime = commit_time_datetime + timedelta(hours=9)
-    
-    # new_commit_time을 원하는 형식으로 변환하여 출력
-    #new_commit_time = new_commit_time_datetime.strftime('%Y-%m-%d %H:%M:%S')
-    
-    # Print the user information
-    print("User Name:", user_name)
-    print("Commit Time:", commit_time)
-    #print("New Commit Time:", new_commit_time)
-    print("Commit Message:", commit_message)
-    print("From Branch:", from_branch)
-    print("To Branch:", to_branch)
-    
-    
-    # 환경 변수 불러오기
-    #origin_branch = os.environ.get('ORIGIN_BRANCH')
-    #branch_name = os.environ.get('BRANCH_NAME')
-    
-    # 환경 변수 출력
-    #print(f"User Name: {user_name}")
-    #print(f"Origin Branch: {origin_branch}")
-    #print(f"Branch Name: {branch_name}")
 
+    
+    
     description=""
     # 조건문을 사용하여 데이터 준비
     if event_name == "Push":
-        description = f"{to_branch}(으)로 push 완료\n'{commit_message}'"
+        description += f"{to_branch}(으)로 push 완료\n'{commit_message}'"
     elif event_name == "Pull Request":
-        description = f"{from_branch}→{to_branch}\n'{commit_message}'"
+        description += f"{from_branch}→{to_branch}\n'{commit_message}'"
 
     
     # 사용자 템플릿 변수에 따라 텍스트, 피드 설정 - 개인 테스트용
     
-    print("description" + description)
+    print("description:" + description)
 
     print("메시지부분시작!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
     
