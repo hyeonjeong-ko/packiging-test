427b1b4ba6ae20e512a4bd3ee2ea7c9ea896cc63
hyeonjeong ko <72601276+hyeonjeong-ko@users.noreply.github.com>
2023-08-22 02:28:14 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 
-echo "Hello entry~!"
+echo "entry point 진입"
 
 # Grant execute permission to all files in the directory
 chmod +x *
@@ -14,35 +14,8 @@ ls
 python -m pip install --upgrade pip
 pip install requests
 
-echo "===============================================명령어 진입==============================================="
+echo "===============================================세부 명령 진입==============================================="
 
-
-# # 이벤트에 따라 유저 정보 설정
-# if [ "$GITHUB_EVENT_NAME" = "push" ]; then
-#   USER_NAME=$(git log -1 --pretty=format:'%an')
-#   COMMIT_TIME=$(git log -1 --pretty=format:'%ci')
-#   COMMIT_MESSAGE=$(git log -1 --pretty=format:'%s')
-#   FROM_BRANCH=$(git symbolic-ref --short HEAD)  # 현재 브랜치 이름 가져오기
-#   TO_BRANCH=${GITHUB_REF#refs/heads/}  # GITHUB_REF에서 "refs/heads/" 접두사 제거
-#   echo "USER_NAME=$USER_NAME" >> $GITHUB_ENV
-#   echo "COMMIT_TIME=$COMMIT_TIME" >> $GITHUB_ENV
-#   echo "COMMIT_MESSAGE=$COMMIT_MESSAGE" >> $GITHUB_ENV
-#   echo "FROM_BRANCH=$FROM_BRANCH" >> $GITHUB_ENV
-#   echo "TO_BRANCH=$TO_BRANCH" >> $GITHUB_ENV
-# fi
-
-# if [ "$GITHUB_EVENT_NAME" = "pull_request" ]; then
-#   USER_NAME=$(git log -1 --pretty=format:'%an')
-#   COMMIT_TIME=$(git log -1 --pretty=format:'%ci')
-#   COMMIT_MESSAGE="${{ github.event.pull_request.title }}"  # 풀 리퀘스트 제목 사용
-#   FROM_BRANCH=${{ github.event.pull_request.head.ref }}
-#   TO_BRANCH=${{ github.event.pull_request.base.ref }}
-#   echo "USER_NAME=$USER_NAME" >> $GITHUB_ENV
-#   echo "COMMIT_TIME=$COMMIT_TIME" >> $GITHUB_ENV
-#   echo "COMMIT_MESSAGE=$COMMIT_MESSAGE" >> $GITHUB_ENV
-#   echo "FROM_BRANCH=$FROM_BRANCH" >> $GITHUB_ENV
-#   echo "TO_BRANCH=$TO_BRANCH" >> $GITHUB_ENV
-# fi
 
 # # 기존 프로필 내용 읽기
 # if [ "$GITHUB_EVENT_NAME" = "push" ]; then
